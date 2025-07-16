5c24897aa06cf5ff404251ed1d2729ac7a1093ca
ericselace <eriselace@gmail.com>
2025-07-16 22:38:59 +0000

    fait

---
+++ scripts/getdates.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const lastModified = document.lastModified;
+    document.getElementById("lastModified").textContent = "Dernière modification : " + lastModified;
+    });
