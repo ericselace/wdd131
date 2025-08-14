ecfbc8d847779c8d5a7424abc30915c4ed884fd2
ericselace <eriselace@gmail.com>
2025-08-14 08:40:42 +0000

    projetfinal

---
+++ project/scripts/helping_hands.js
@@ -0,0 +1,12 @@
+const currentYear = new Date().getFullYear();
+document.querySelector("#current-year").textContent = currentYear;
+
+
+const lastModified = document.lastModified;
+document.querySelector("#last-modified").textContent = `Last Modified: ${lastModified}`;
+
+
+
+
+
+
